1c930a546cedc2308abd61d28dad3ff40e5ee1b1
Brian Menard <brian@webdevbrian.com>
2016-05-31 20:00:49 +0000

    added link color to potfolio, social to navbar

---
+++ js/buttonUpdater.js
@@ -0,0 +1,9 @@
+$('.webAnimation').click(function(){
+    $(this).css({'color':'#E94847'});
+    $('.designAnimation').css({'color':'black'});
+});
+
+$('.designAnimation').click(function(){
+    $(this).css({'color':'#E94847'});
+    $('.webAnimation').css({'color':'black'});
+});
