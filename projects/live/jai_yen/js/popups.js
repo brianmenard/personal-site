82f2035951b860a49c989df2128621a89e03d013
Brian Menard <brian@webdevbrian.com>
2016-12-13 15:52:05 +0000

    added jai_yen to live projects

---
+++ projects/live/jai_yen/js/popups.js
@@ -0,0 +1,53 @@
+	$(document).ready(function () {
+	
+	    $('#bill').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#jim').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#chris').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#janet').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test1').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test2').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test3').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test4').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test5').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test6').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	
+	});
